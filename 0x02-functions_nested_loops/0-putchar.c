92d8ee8172d697bcd1088aecca6ef9615ebe77cd
mimo-oo <fadipemichael6@gmail.com>
2024-06-25 13:37:37 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include main.h
 /**
  * main - this is the entry point
  * Return: 0 (success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
 	return (0);
 }
 
