37d64e575bc3eead0ca2636ffd9efe8a7a2b1b41
mimo-oo <fadipemichael6@gmail.com>
2024-07-01 14:11:39 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the value of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		return (i);
+	}
+}
