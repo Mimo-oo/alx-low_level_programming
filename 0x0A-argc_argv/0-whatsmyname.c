0b4d618ef0848ba4057f6f921aee891130207a93
mimo-oo <fadipemichael6@gmail.com>
2024-07-12 12:10:18 -0700

    name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the name eof a program
+ * @argc: number of command line arguements
+ * @argv: name of array
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
