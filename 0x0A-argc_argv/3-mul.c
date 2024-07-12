85b25c67bf95db347baee4de684afcefc0b353e7
mimo-oo <fadipemichael6@gmail.com>
2024-07-12 12:44:30 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - multiply the arguments
+ * @argc: number of arguemtns
+ * @argv: name of array
+ * Return: 0 success
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
+
+	}
+}
