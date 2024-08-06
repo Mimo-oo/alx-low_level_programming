e3f80086eb69e6b2f5c8ed1b1846d6de897e12d5
mimo-oo <fadipemichael6@gmail.com>
2024-08-06 13:59:30 -0700

    free list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,9 +5,6 @@
  * print_listint - function that prints all the elements of a listint_t list.
  * @h: pointer to the head of linked list.
  *
- * Format: see example.
- * You are allowed to use printf.
- *
  * Return: all the elements of the list.
  */
 
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,9 +5,6 @@
  * free_listint2 - function that frees a singly linked list.
  * @head: pointer to pointer to the head of linked list.
  *
- * Make sure there is no memory leaks.
- * The function sets the head to NULL.
- *
  * Return: No return.
  */
 
@@ -16,7 +13,7 @@ void free_listint2(listint_t **head)
 	listint_t *temp_variable;
 
 	if (head == NULL)
-		return; /* do not do anything */
+		return;
 	while (*head != NULL)
 	{
 		temp_variable =  *head;
